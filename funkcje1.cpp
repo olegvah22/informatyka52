bf32c0012f88afe2fdc83c89a113937d4dce46ec
olegvah22 <olegvahila@gmail.com>
2026-03-16 14:08:38 +0100

    Komentarz

---
+++ funkcje1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+int N;{
+cout << "podaj liczbe calkowita:" <<endl;
+ cin >> N;
+
+
+
+cout << "liczba " << N << " jest parzysta"<<endl;
+
+
+
+}
+
+
+return 0;
+
+}
---
+++ funkcjer1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int Dzielic(int suma) {
+	
+bool czyparzysta(int liczba){
+
+if (suma % 2 == 0 ){
+return true;
+ } else {
+return false;
+  }
+}
+ 
+   return suma/2;
+
+}
+
+int main() {
+
+
+
+
+int N;
+cout << "podaj Liczba calkowito :" <<endl;	
+	cin >> N;
+	int wynik = Dzielic(N);
+
+
+
+	
+
+    return 0;
+}
